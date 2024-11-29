cb5adedd50b29360c0eb3bfffce624530fc9cdd9
Bhuvana <151706146+bhuvana-chandran@users.noreply.github.com>
2024-11-29 13:38:18 +0530

    first commit

--- backend/dist/index.js
+++
@@ -1,9 +0,0 @@
-import app from "./app.js";
-import { connectToDataBase } from "./db/connection.js";
-const PORT = process.env.PORT || 5000;
-connectToDataBase()
-    .then(() => {
-    app.listen(PORT, () => console.log("Server Connected to the PORT Successfully & Connected DataBase Successfully!"));
-})
-    .catch((error) => console.log(error));
-//# sourceMappingURL=index.js.map
--- backend/dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import UserRouters from "./UserRouter.js";
-import ChatRouters from "./ChatRouter.js";
-const appRouter = Router();
-appRouter.use("/user", UserRouters); // domain/api/v1/user/
-appRouter.use("/chat", ChatRouters);
-export default appRouter;
-//# sourceMappingURL=index.js.map
